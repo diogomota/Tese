ad6a6bb27758912dee77817e1f5fd1f4bc1805a7
diogomota <ec12135@fe.up.pt>
2017-03-08 10:20:12 +0000

    Individuals are now store in matrix
    The sorting algorithm is more efficient in a matrix (vs list)

--- Main_app/Fraser/Fraser/Population.cs
+++ Main_app/Fraser/Fraser/Population.cs
@@ -8,17 +8,18 @@ namespace Fraser
 {
    public class Population
     {
-       // public Individual[] ind;
-        public List<Individual> ind;
+        public Individual[] ind;
+        //public List<Individual> ind;
         static public Random rand = new Random(0); //o random seeder so se inicia uma vez para a população
 
         public Population (int max_pop, Genome _baseDNA){
-            //this.ind = new Individual[max_pop];
-            ind = new List<Individual>();
+            this.ind = new Individual[max_pop];
+
+            //ind = new List<Individual>();
             for (int i = 0; i < max_pop; i++)
             {
                 
-                ind.Add(new Individual(_baseDNA, ref rand));
+                ind[i]=new Individual(_baseDNA, ref rand);
 
             }
         }
